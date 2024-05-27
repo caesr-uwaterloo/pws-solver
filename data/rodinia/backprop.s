79dec346e1c4057958b9d9d22c9ae0a8fd88b0b8
Artem Klashtorny <aklashto@uwaterloo.ca>
2024-05-27 19:16:58 -0400

    Updated backprop data

--- data/rodinia/backprop.s
+++ data/rodinia/backprop.s
@@ -11,45 +11,54 @@ _Z22bpnn_layerforward_CUDAPfS_S_S_ii$local:
 	s_load_dword s2, s[4:5], 0x24
 	s_lshl_b32 s0, s7, 4
 	v_add_u32_e32 v2, vcc, s0, v1
-	v_cmp_eq_u32_e32 vcc, 0, v0
+	v_cmp_eq_u32_e64 s[0:1], 0, v0
 	v_lshlrev_b32_e32 v5, 2, v1
 	s_mov_b32 m0, -1
-	s_and_saveexec_b64 s[12:13], vcc
+	s_and_saveexec_b64 s[12:13], s[0:1]
 	s_cbranch_execz BB0_2
 ; %bb.1:
 	s_load_dwordx2 s[4:5], s[4:5], 0x0
-	v_add_u32_e64 v3, s[0:1], 1, v2
+	v_add_u32_e32 v3, vcc, 1, v2
 	v_ashrrev_i32_e32 v4, 31, v3
 	v_lshlrev_b64 v[3:4], 2, v[3:4]
 	s_waitcnt lgkmcnt(0)
 	v_mov_b32_e32 v6, s5
-	v_add_u32_e64 v3, s[0:1], s4, v3
-	v_addc_u32_e64 v4, s[0:1], v6, v4, s[0:1]
+	v_add_u32_e32 v3, vcc, s4, v3
+	v_addc_u32_e32 v4, vcc, v6, v4, vcc
 	flat_load_dword v3, v[3:4]
 	s_waitcnt vmcnt(0) lgkmcnt(0)
 	ds_write_b32 v5, v3
 BB0_2:
 	s_or_b64 exec, exec, s[12:13]
 	s_waitcnt lgkmcnt(0)
-	s_add_i32 s0, s2, 1
-	v_mul_lo_u32 v2, v2, s0
-	v_add_u32_e64 v3, s[0:1], s0, v0
-	v_add_u32_e64 v2, s[0:1], v3, v2
-	v_add_u32_e64 v2, s[0:1], 1, v2
+	s_add_i32 s3, s2, 1
+	v_mul_lo_u32 v2, v2, s3
+	v_add_u32_e32 v3, vcc, s3, v0
+	v_add_u32_e32 v2, vcc, v3, v2
+	v_add_u32_e32 v2, vcc, 1, v2
 	s_waitcnt lgkmcnt(0)
 	s_barrier
 	s_waitcnt lgkmcnt(0)
 	v_ashrrev_i32_e32 v3, 31, v2
 	v_lshlrev_b64 v[2:3], 2, v[2:3]
 	v_mov_b32_e32 v4, s9
-	v_add_u32_e64 v2, s[0:1], s8, v2
-	v_addc_u32_e64 v3, s[0:1], v4, v3, s[0:1]
+	v_add_u32_e32 v2, vcc, s8, v2
+	v_addc_u32_e32 v3, vcc, v4, v3, vcc
 	flat_load_dword v6, v[2:3]
+	s_mov_b32 s3, 1
 	v_lshlrev_b32_e32 v4, 2, v0
 	v_lshlrev_b32_e32 v7, 6, v1
-	v_and_b32_e32 v8, 1, v1
-	v_add_u32_e64 v7, s[0:1], v7, v4
-	v_add_u32_e64 v4, s[0:1], 64, v7
+	s_mov_b32 s4, 0x3f317218
+	s_mov_b32 s5, 0xb102e308
+	s_movk_i32 s6, 0x204
+	s_mov_b32 s8, 0x7f800000
+	s_mov_b32 s9, 0x3ab42872
+	s_mov_b32 s12, 0x3d2aadcc
+	s_mov_b32 s13, 0x3e2aaa47
+	s_mov_b32 s14, 0x3efffffc
+	s_mov_b32 s15, 0x42b20000
+	v_add_u32_e32 v7, vcc, v7, v4
+	v_add_u32_e32 v4, vcc, 64, v7
 	s_waitcnt vmcnt(0) lgkmcnt(0)
 	ds_write_b32 v7, v6 offset:64
 	s_waitcnt lgkmcnt(0)
@@ -64,71 +73,136 @@ BB0_2:
 	s_waitcnt lgkmcnt(0)
 	s_barrier
 	s_waitcnt lgkmcnt(0)
-	v_cmp_eq_u32_e64 s[0:1], 0, v8
-	s_and_saveexec_b64 s[4:5], s[0:1]
-	s_cbranch_execz BB0_4
-; %bb.3:
-	ds_read2_b32 v[5:6], v4 offset1:16
-	s_waitcnt lgkmcnt(0)
-	v_add_f32_e32 v5, v5, v6
-	ds_write_b32 v4, v5
-BB0_4:
-	s_or_b64 exec, exec, s[4:5]
+	s_mov_b32 s16, 0xc2d00000
+	s_branch BB0_4
+BB0_3:                                  ;   in Loop: Header=BB0_4 Depth=1
+	s_or_b64 exec, exec, s[18:19]
 	s_waitcnt lgkmcnt(0)
 	s_barrier
+	s_add_i32 s3, s3, 1
+	s_cmp_eq_u32 s3, 5
 	s_waitcnt lgkmcnt(0)
-	v_and_b32_e32 v5, 3, v1
-	v_cmp_eq_u32_e64 s[0:1], 0, v5
-	s_and_saveexec_b64 s[4:5], s[0:1]
-	s_cbranch_execz BB0_6
-; %bb.5:
-	ds_read2_b32 v[5:6], v4 offset1:32
+	s_cbranch_scc1 BB0_6
+BB0_4:                                  ; =>This Inner Loop Header: Depth=1
+	v_cvt_f32_i32_e32 v5, s3
+	v_mul_f32_e32 v6, s4, v5
+	v_fma_f32 v7, v5, s4, -v6
+	v_fma_f32 v5, v5, s5, v7
+	v_add_f32_e32 v7, v6, v5
+	v_sub_f32_e32 v8, v7, v6
+	v_cmp_class_f32_e64 vcc, v6, s6
+	v_cndmask_b32_e32 v6, v7, v6, vcc
+	v_sub_f32_e32 v5, v5, v8
+	v_cmp_neq_f32_e64 vcc, |v6|, s8
+	v_cndmask_b32_e32 v5, 0, v5, vcc
+	v_mul_f32_e32 v7, 0x3fb8aa3b, v6
+	v_rndne_f32_e32 v7, v7
+	v_mul_f32_e32 v8, 0x3f317200, v7
+	v_sub_f32_e32 v9, v6, v8
+	v_sub_f32_e32 v10, v9, v6
+	v_sub_f32_e32 v11, v9, v10
+	v_sub_f32_e32 v11, v6, v11
+	v_add_f32_e32 v8, v8, v10
+	v_sub_f32_e32 v8, v11, v8
+	v_add_f32_e32 v5, v5, v8
+	v_add_f32_e32 v8, v9, v5
+	v_sub_f32_e32 v9, v8, v9
+	v_sub_f32_e32 v5, v5, v9
+	v_mul_f32_e32 v9, 0x35bfbc00, v7
+	v_sub_f32_e32 v10, v8, v9
+	v_sub_f32_e32 v8, v8, v10
+	v_sub_f32_e32 v8, v8, v9
+	v_add_f32_e32 v5, v5, v8
+	v_add_f32_e32 v8, v10, v5
+	v_sub_f32_e32 v9, v8, v10
+	v_sub_f32_e32 v5, v5, v9
+	v_mul_f32_e32 v9, 0x2ea39ef3, v7
+	v_sub_f32_e32 v10, v8, v9
+	v_sub_f32_e32 v8, v8, v10
+	v_sub_f32_e32 v8, v8, v9
+	v_add_f32_e32 v5, v5, v8
+	v_add_f32_e32 v8, v10, v5
+	v_sub_f32_e32 v9, v8, v10
+	v_sub_f32_e32 v5, v5, v9
+	v_mov_b32_e32 v9, 0x3c091de6
+	v_fma_f32 v9, v8, s9, v9
+	v_fma_f32 v9, v8, v9, s12
+	v_fma_f32 v9, v8, v9, s13
+	v_fma_f32 v9, v8, v9, s14
+	v_mul_f32_e32 v10, v8, v8
+	v_fma_f32 v11, v8, v8, -v10
+	v_add_f32_e32 v12, v5, v5
+	v_fma_f32 v11, v8, v12, v11
+	v_fma_f32 v11, v5, v5, v11
+	v_add_f32_e32 v12, v10, v11
+	v_mul_f32_e32 v13, v9, v12
+	v_sub_f32_e32 v10, v12, v10
+	v_sub_f32_e32 v10, v11, v10
+	v_fma_f32 v11, v12, v9, -v13
+	v_fma_f32 v9, v10, v9, v11
+	v_add_f32_e32 v10, v13, v9
+	v_sub_f32_e32 v11, v10, v13
+	v_sub_f32_e32 v9, v9, v11
+	v_add_f32_e32 v11, v8, v10
+	v_sub_f32_e32 v8, v11, v8
+	v_sub_f32_e32 v8, v10, v8
+	v_add_f32_e32 v5, v5, v9
+	v_cvt_i32_f32_e32 v7, v7
+	v_add_f32_e32 v5, v8, v5
+	v_add_f32_e32 v5, v11, v5
+	v_add_f32_e32 v5, 1.0, v5
+	v_ldexp_f32 v5, v5, v7
+	v_mov_b32_e32 v7, s8
+	v_cmp_nlt_f32_e32 vcc, s15, v6
+	v_cndmask_b32_e32 v5, v7, v5, vcc
+	v_cmp_ngt_f32_e32 vcc, s16, v6
+	v_cndmask_b32_e32 v5, 0, v5, vcc
+	v_cvt_i32_f32_e64 v5, |v5|
+	v_ashrrev_i32_e32 v6, 31, v5
+	v_add_u32_e32 v7, vcc, v5, v6
+	v_xor_b32_e32 v6, v7, v6
+	v_cvt_f32_u32_e32 v7, v6
+	v_rcp_iflag_f32_e32 v7, v7
+	v_mul_f32_e32 v7, 0x4f7ffffe, v7
+	v_cvt_u32_f32_e32 v7, v7
+	v_sub_u32_e32 v8, vcc, 0, v6
+	v_mul_lo_u32 v8, v8, v7
+	v_mul_hi_u32 v8, v7, v8
+	v_add_u32_e32 v7, vcc, v8, v7
+	v_mul_hi_u32 v7, v1, v7
+	v_mul_lo_u32 v7, v7, v6
+	v_sub_u32_e32 v7, vcc, v1, v7
+	v_sub_u32_e32 v8, vcc, v7, v6
+	v_cmp_ge_u32_e32 vcc, v7, v6
+	v_cndmask_b32_e32 v7, v7, v8, vcc
+	v_sub_u32_e32 v8, vcc, v7, v6
+	v_cmp_ge_u32_e32 vcc, v7, v6
+	v_cndmask_b32_e32 v6, v7, v8, vcc
+	v_cmp_eq_u32_e32 vcc, 0, v6
+	s_and_saveexec_b64 s[18:19], vcc
+	s_cbranch_execz BB0_3
+; %bb.5:                                ;   in Loop: Header=BB0_4 Depth=1
+	v_lshrrev_b32_e32 v6, 31, v5
+	v_add_u32_e32 v5, vcc, v6, v5
+	v_lshlrev_b32_e32 v5, 5, v5
+	v_and_b32_e32 v5, 0xffffffc0, v5
+	v_add_u32_e32 v5, vcc, v4, v5
+	ds_read_b32 v6, v4
+	ds_read_b32 v5, v5
 	s_waitcnt lgkmcnt(0)
-	v_add_f32_e32 v5, v5, v6
+	v_add_f32_e32 v5, v6, v5
 	ds_write_b32 v4, v5
+	s_branch BB0_3
 BB0_6:
-	s_or_b64 exec, exec, s[4:5]
-	s_waitcnt lgkmcnt(0)
-	s_barrier
-	s_waitcnt lgkmcnt(0)
-	v_and_b32_e32 v5, 7, v1
-	v_cmp_eq_u32_e64 s[0:1], 0, v5
-	s_and_saveexec_b64 s[4:5], s[0:1]
-	s_cbranch_execz BB0_8
-; %bb.7:
-	ds_read2st64_b32 v[5:6], v4 offset1:1
-	s_waitcnt lgkmcnt(0)
-	v_add_f32_e32 v5, v5, v6
-	ds_write_b32 v4, v5
-BB0_8:
-	s_or_b64 exec, exec, s[4:5]
-	s_waitcnt lgkmcnt(0)
-	s_barrier
-	s_waitcnt lgkmcnt(0)
-	v_and_b32_e32 v5, 15, v1
-	v_cmp_eq_u32_e64 s[0:1], 0, v5
-	s_and_saveexec_b64 s[4:5], s[0:1]
-	s_cbranch_execz BB0_10
-; %bb.9:
-	ds_read2st64_b32 v[5:6], v4 offset1:2
-	s_waitcnt lgkmcnt(0)
-	v_add_f32_e32 v5, v5, v6
-	ds_write_b32 v4, v5
-BB0_10:
-	s_or_b64 exec, exec, s[4:5]
-	s_waitcnt lgkmcnt(0)
-	s_barrier
-	s_waitcnt lgkmcnt(0)
 	ds_read_b32 v4, v4
 	s_waitcnt lgkmcnt(0)
 	flat_store_dword v[2:3], v4
 	s_waitcnt vmcnt(0) lgkmcnt(0)
 	s_barrier
 	s_waitcnt lgkmcnt(0)
-	s_and_saveexec_b64 s[0:1], vcc
-	s_xor_b64 s[0:1], exec, s[0:1]
-	s_cbranch_execz BB0_12
-; %bb.11:
+	s_and_saveexec_b64 s[4:5], s[0:1]
+	s_cbranch_execz BB0_8
+; %bb.7:
 	v_lshlrev_b32_e32 v2, 2, v1
 	v_lshlrev_b32_e32 v0, 6, v0
 	v_add_u32_e32 v2, vcc, v0, v2
@@ -142,7 +216,7 @@ BB0_10:
 	ds_read_b32 v2, v2 offset:64
 	s_waitcnt lgkmcnt(0)
 	flat_store_dword v[0:1], v2
-BB0_12:
+BB0_8:
 	s_endpgm
 	.section	.rodata,#alloc
 	.p2align	6
@@ -162,8 +236,8 @@ BB0_12:
 		.amdhsa_system_sgpr_workgroup_id_z 0
 		.amdhsa_system_sgpr_workgroup_info 0
 		.amdhsa_system_vgpr_workitem_id 1
-		.amdhsa_next_free_vgpr 9
-		.amdhsa_next_free_sgpr 14
+		.amdhsa_next_free_vgpr 14
+		.amdhsa_next_free_sgpr 20
 		.amdhsa_reserve_flat_scratch 0
 		.amdhsa_float_round_mode_32 0
 		.amdhsa_float_round_mode_16_64 0
@@ -185,18 +259,18 @@ BB0_12:
                                         ; -- End function
 	.section	.AMDGPU.csdata
 ; Kernel info:
-; codeLenInByte = 660
-; NumSgprs: 18
-; NumVgprs: 9
+; codeLenInByte = 1040
+; NumSgprs: 24
+; NumVgprs: 14
 ; ScratchSize: 0
 ; MemoryBound: 0
 ; FloatMode: 240
 ; IeeeMode: 1
 ; LDSByteSize: 1088 bytes/workgroup (compile time only)
 ; SGPRBlocks: 2
-; VGPRBlocks: 2
-; NumSGPRsForWavesPerEU: 18
-; NumVGPRsForWavesPerEU: 9
+; VGPRBlocks: 3
+; NumSGPRsForWavesPerEU: 24
+; NumVGPRsForWavesPerEU: 14
 ; Occupancy: 10
 ; WaveLimiterHint : 1
 ; COMPUTE_PGM_RSRC2:USER_SGPR: 6
@@ -446,10 +520,10 @@ amdhsa.kernels:
     .max_flat_workgroup_size: 256
     .name:           _Z22bpnn_layerforward_CUDAPfS_S_S_ii
     .private_segment_fixed_size: 0
-    .sgpr_count:     18
+    .sgpr_count:     24
     .sgpr_spill_count: 0
     .symbol:         _Z22bpnn_layerforward_CUDAPfS_S_S_ii.kd
-    .vgpr_count:     9
+    .vgpr_count:     14
     .vgpr_spill_count: 0
     .wavefront_size: 64
   - .args:
