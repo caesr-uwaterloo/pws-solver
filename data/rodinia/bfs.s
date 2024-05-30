ef2c51022c7fd9eb223e16ca5269a0ee4233d216
Artem Klashtorny <aklashto@uwaterloo.ca>
2024-05-29 21:12:53 -0400

    Updated bfs files

--- data/rodinia/bfs.s
+++ data/rodinia/bfs.s
@@ -10,95 +10,95 @@ _Z6KernelP4NodePiPbS2_S2_S1_i$local:
 	s_load_dword s2, s[4:5], 0x30
 	s_mov_b32 s7, 0
 	s_lshl_b64 s[0:1], s[6:7], 9
-	v_or_b32_e32 v2, s0, v0
+	v_or_b32_e32 v5, s0, v0
 	s_waitcnt lgkmcnt(0)
-	v_cmp_gt_i32_e32 vcc, s2, v2
+	v_cmp_gt_i32_e32 vcc, s2, v5
 	s_and_saveexec_b64 s[0:1], vcc
 	s_cbranch_execz BB0_7
 ; %bb.1:
 	s_load_dwordx8 s[8:15], s[4:5], 0x0
-	v_ashrrev_i32_e32 v3, 31, v2
+	v_ashrrev_i32_e32 v6, 31, v5
 	s_waitcnt lgkmcnt(0)
 	v_mov_b32_e32 v1, s13
-	v_add_u32_e32 v0, vcc, s12, v2
-	v_addc_u32_e32 v1, vcc, v1, v3, vcc
-	flat_load_ubyte v4, v[0:1]
+	v_add_u32_e32 v0, vcc, s12, v5
+	v_addc_u32_e32 v1, vcc, v1, v6, vcc
+	flat_load_ubyte v2, v[0:1]
 	s_waitcnt vmcnt(0) lgkmcnt(0)
-	v_cmp_ne_u16_e32 vcc, 0, v4
+	v_cmp_ne_u16_e32 vcc, 0, v2
 	s_and_b64 exec, exec, vcc
 	s_cbranch_execz BB0_7
 ; %bb.2:
-	s_load_dwordx4 s[0:3], s[4:5], 0x20
-	v_mov_b32_e32 v4, 0
-	s_waitcnt lgkmcnt(0)
-	flat_store_byte v[0:1], v4
-	v_lshlrev_b64 v[0:1], 3, v[2:3]
-	v_mov_b32_e32 v4, s9
+	v_mov_b32_e32 v2, 0
+	flat_store_byte v[0:1], v2
+	v_lshlrev_b64 v[0:1], 3, v[5:6]
+	v_mov_b32_e32 v2, s9
 	v_add_u32_e32 v0, vcc, s8, v0
-	v_addc_u32_e32 v1, vcc, v4, v1, vcc
+	v_addc_u32_e32 v1, vcc, v2, v1, vcc
 	v_add_u32_e32 v0, vcc, 4, v0
 	v_addc_u32_e32 v1, vcc, 0, v1, vcc
-	flat_load_dword v0, v[0:1]
+	flat_load_dword v1, v[0:1]
+	s_load_dwordx4 s[0:3], s[4:5], 0x20
 	s_waitcnt vmcnt(0) lgkmcnt(0)
-	v_cmp_lt_i32_e32 vcc, 0, v0
+	v_cmp_lt_i32_e32 vcc, 0, v1
 	s_and_b64 exec, exec, vcc
 	s_cbranch_execz BB0_7
 ; %bb.3:                                ; %.lr.ph
-	v_lshlrev_b64 v[0:1], 3, v[2:3]
-	v_mov_b32_e32 v4, s9
-	v_add_u32_e32 v0, vcc, s8, v0
-	v_addc_u32_e32 v1, vcc, v4, v1, vcc
-	flat_load_dword v4, v[0:1]
-	v_lshlrev_b64 v[2:3], 2, v[2:3]
-	v_mov_b32_e32 v5, s3
-	v_mov_b32_e32 v7, s11
-	v_add_u32_e32 v2, vcc, s2, v2
-	v_addc_u32_e32 v3, vcc, v5, v3, vcc
+	v_lshlrev_b64 v[2:3], 3, v[5:6]
+	v_mov_b32_e32 v0, s9
+	v_add_u32_e32 v2, vcc, s8, v2
+	v_addc_u32_e32 v3, vcc, v0, v3, vcc
+	flat_load_dword v4, v[2:3]
+	v_lshlrev_b64 v[6:7], 2, v[5:6]
+	v_mov_b32_e32 v0, s3
+	v_add_u32_e32 v6, vcc, s2, v6
+	v_addc_u32_e32 v7, vcc, v0, v7, vcc
+	v_mov_b32_e32 v10, s11
+	s_mov_b64 s[4:5], 0
 	s_waitcnt vmcnt(0) lgkmcnt(0)
 	v_ashrrev_i32_e32 v5, 31, v4
-	v_lshlrev_b64 v[5:6], 2, v[4:5]
-	v_add_u32_e32 v5, vcc, s10, v5
-	v_addc_u32_e32 v6, vcc, v7, v6, vcc
-	s_mov_b64 s[4:5], 0
+	v_lshlrev_b64 v[8:9], 2, v[4:5]
+	v_mov_b32_e32 v0, v4
+	v_add_u32_e32 v8, vcc, s10, v8
+	v_addc_u32_e32 v9, vcc, v10, v9, vcc
 	s_branch BB0_5
 BB0_4:                                  ;   in Loop: Header=BB0_5 Depth=1
 	s_or_b64 exec, exec, s[6:7]
-	flat_load_dwordx2 v[7:8], v[0:1]
 	v_add_u32_e32 v4, vcc, 1, v4
 	s_waitcnt vmcnt(0) lgkmcnt(0)
-	v_add_u32_e32 v7, vcc, v7, v8
-	v_cmp_ge_i32_e32 vcc, v4, v7
+	v_add_u32_e32 v5, vcc, v0, v1
+	v_cmp_ge_i32_e32 vcc, v4, v5
 	s_or_b64 s[4:5], vcc, s[4:5]
-	v_add_u32_e32 v5, vcc, 4, v5
-	v_addc_u32_e32 v6, vcc, 0, v6, vcc
+	v_add_u32_e32 v8, vcc, 4, v8
+	v_addc_u32_e32 v9, vcc, 0, v9, vcc
 	s_andn2_b64 exec, exec, s[4:5]
 	s_cbranch_execz BB0_7
 BB0_5:                                  ; =>This Inner Loop Header: Depth=1
-	flat_load_dword v7, v[5:6]
-	v_mov_b32_e32 v10, s1
+	flat_load_dword v10, v[8:9]
+	v_mov_b32_e32 v5, s1
 	s_waitcnt vmcnt(0) lgkmcnt(0)
-	v_ashrrev_i32_e32 v8, 31, v7
-	v_add_u32_e32 v9, vcc, s0, v7
-	v_addc_u32_e32 v10, vcc, v10, v8, vcc
-	flat_load_ubyte v9, v[9:10]
+	v_ashrrev_i32_e32 v11, 31, v10
+	v_add_u32_e32 v12, vcc, s0, v10
+	v_addc_u32_e32 v13, vcc, v5, v11, vcc
+	flat_load_ubyte v5, v[12:13]
 	s_waitcnt vmcnt(0) lgkmcnt(0)
-	v_cmp_eq_u16_e32 vcc, 0, v9
+	v_cmp_eq_u16_e32 vcc, 0, v5
 	s_and_saveexec_b64 s[6:7], vcc
 	s_cbranch_execz BB0_4
 ; %bb.6:                                ;   in Loop: Header=BB0_5 Depth=1
-	flat_load_dword v11, v[2:3]
-	v_lshlrev_b64 v[9:10], 2, v[7:8]
-	v_mov_b32_e32 v12, s3
-	v_mov_b32_e32 v13, s15
+	flat_load_dword v5, v[6:7]
+	v_lshlrev_b64 v[0:1], 2, v[10:11]
+	v_mov_b32_e32 v12, s15
+	v_add_u32_e32 v10, vcc, s14, v10
+	v_addc_u32_e32 v11, vcc, v12, v11, vcc
+	v_mov_b32_e32 v13, s3
+	v_add_u32_e32 v0, vcc, s2, v0
+	v_addc_u32_e32 v1, vcc, v13, v1, vcc
 	v_mov_b32_e32 v14, 1
-	v_add_u32_e32 v7, vcc, s14, v7
-	v_addc_u32_e32 v8, vcc, v13, v8, vcc
-	v_add_u32_e32 v9, vcc, s2, v9
-	v_addc_u32_e32 v10, vcc, v12, v10, vcc
 	s_waitcnt vmcnt(0) lgkmcnt(0)
-	v_add_u32_e32 v11, vcc, 1, v11
-	flat_store_dword v[9:10], v11
-	flat_store_byte v[7:8], v14
+	v_add_u32_e32 v5, vcc, 1, v5
+	flat_store_dword v[0:1], v5
+	flat_store_byte v[10:11], v14
+	flat_load_dwordx2 v[0:1], v[2:3]
 	s_branch BB0_4
 BB0_7:                                  ; %.loopexit
 	s_endpgm
@@ -192,17 +192,17 @@ _Z7Kernel2PbS_S_S_i$local:
 	s_and_b64 exec, exec, vcc
 	s_cbranch_execz BB1_3
 ; %bb.2:
-	v_mov_b32_e32 v4, s6
-	v_mov_b32_e32 v5, s7
-	v_mov_b32_e32 v7, s1
-	v_add_u32_e32 v6, vcc, s0, v0
+	v_mov_b32_e32 v7, s5
+	v_add_u32_e32 v6, vcc, s4, v0
 	v_addc_u32_e32 v7, vcc, v7, v3, vcc
-	v_mov_b32_e32 v9, s5
-	v_add_u32_e32 v8, vcc, s4, v0
+	v_add_u32_e32 v8, vcc, s0, v0
+	v_mov_b32_e32 v9, s1
 	v_addc_u32_e32 v9, vcc, v9, v3, vcc
 	v_mov_b32_e32 v0, 1
-	flat_store_byte v[6:7], v0
+	v_mov_b32_e32 v4, s6
+	v_mov_b32_e32 v5, s7
 	flat_store_byte v[8:9], v0
+	flat_store_byte v[6:7], v0
 	flat_store_byte v[4:5], v0
 	v_mov_b32_e32 v0, 0
 	flat_store_byte v[1:2], v0
